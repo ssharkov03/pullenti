852c2009783298fb92c36273dbc8228784fbd84a
ssharkov03 <serezhasharkov2003@gmail.com>
2023-02-16 04:10:21 +0700

    last

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="PullentiPython",
-    version="0.0.5",
+    version="0.0.6",
     author="Sergei Sharkov",
     author_email="serezhasharkov2003@gmail.com",
     url='https://github.com/ssharkov03/PullentiPython',  # Provide either the link to your github or to your website
-    download_url='https://github.com/ssharkov03/PullentiPython/archive/refs/tags/0.0.5.tar.gz',  # I explain this later on
+    download_url='https://github.com/ssharkov03/PullentiPython/archive/refs/tags/0.0.6.tar.gz',  # I explain this later on
     description="Python implementation of PullentiPython.",
     packages=find_packages(),
     python_requires=">=3.8"
