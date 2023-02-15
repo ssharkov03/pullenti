451bbb03dc1909b342d95346bc37d702e61c22c9
ssharkov03 <serezhasharkov2003@gmail.com>
2023-02-16 03:02:49 +0700

    first commit

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Sergei Sharkov",
     author_email="serezhasharkov2003@gmail.com",
     url='https://github.com/ssharkov03/PullentiPython',  # Provide either the link to your github or to your website
-    download_url='https://github.com/ssharkov03/PullentiPython/archive/refs/tags/0.0.1.tar.gz',  # I explain this later on
+    download_url='https://github.com/ssharkov03/PullentiPython/archive/refs/tags/0.0.2.tar.gz',  # I explain this later on
     description="Python implementation of PullentiPython.",
     package_dir={"": "pullenti"},
     packages=find_packages(where="pullenti"),
