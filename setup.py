fd3f00fcac5593080cbff3e613461dafa0b848f1
ssharkov03 <serezhasharkov2003@gmail.com>
2023-02-16 13:29:44 +0700

    last

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     url='https://github.com/ssharkov03/pullenti',  # Provide either the link to your github or to your website
     download_url='https://github.com/ssharkov03/pullenti/archive/refs/tags/0.0.7.tar.gz',  # I explain this later on
     packages=find_packages(),
+    include_package_data=True,
     python_requires=">=3.8"
 )
 
