e351bbd28b27c2f48aeaeb6e1dd8712b2a0b46cb
ssharkov03 <serezhasharkov2003@gmail.com>
2023-02-16 13:59:18 +0700

    last

--- setup.py
+++ setup.py
@@ -2,14 +2,23 @@ from setuptools import setup, find_packages
 
 setup(
     name="PullentiPython-ssharkov",
-    version="0.0.8",
+    version="0.0.9",
     author="Sergei Sharkov",
     author_email="serezhasharkov2003@gmail.com",
     description="Python implementation of PullentiPython.",
     url='https://github.com/ssharkov03/pullenti',  # Provide either the link to your github or to your website
-    download_url='https://github.com/ssharkov03/pullenti/archive/refs/tags/0.0.8.tar.gz',  # I explain this later on
+    download_url='https://github.com/ssharkov03/pullenti/archive/refs/tags/0.0.9.tar.gz',  # I explain this later on
     packages=find_packages(),
     include_package_data=True,
+    package_data={
+            'pullenti': [
+                '**/*.dat',
+                '**/*.csv',
+                '**/*.txt',
+                '**/*.jpg',
+                '**/*.png'
+            ]
+        },
     python_requires=">=3.8"
 )
 
